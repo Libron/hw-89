384f88d97a6e5944150e5ce42e62fa260721f329
Urmat Bekboev <bek.urmat@gmail.com>
2019-05-21 17:49:00 +0600

    #94 HomeWork

--- api/app/tracks.js
+++ api/app/tracks.js
@@ -74,7 +74,7 @@ router.post('/', [auth, permit('user', 'admin')], (req, res) => {
     }
 });
 
-router.post('/:id/toggle_publish', [auth, permit('user', 'admin')], async (req, res) => {
+router.post('/:id/toggle_publish', [auth, permit('admin')], async (req, res) => {
     const track = await Track.findById(req.params.id);
 
     if (!track) {
