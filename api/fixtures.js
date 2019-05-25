44c40a80d87b8916ccc5d59cb90c167fd9ca8730
Urmat Bekboev <bek.urmat@gmail.com>
2019-05-25 18:17:11 +0600

    #94 HomeWork

--- api/fixtures.js
+++ api/fixtures.js
@@ -43,7 +43,7 @@ const run = async () => {
 
     const [user, admin] = await User.create(
         {
-            username: 'bpycorigbj_1558718709@tfbnw.net',
+            username: 'bpycorsigbj_1558718709@tfbnw.net',
             facebookId: '100037302691339',
             displayName: 'James Alcgcjbfiacci Greeneman',
             password: '111aaa',
@@ -52,7 +52,7 @@ const run = async () => {
             avatarImage: 'https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=109980593577521&height=50&width=50&ext=1561277553&hash=AeQt7Ybkjlpi2F6W'
         },
         {
-            username: 'rgzspaivls_1558718705@tfbnw.net',
+            username: 'rgzspaisvls_1558718705@tfbnw.net',
             facebookId: '100037109025054',
             displayName: 'Helen Alcgajijbejed Lausen',
             password: '111aaa',
